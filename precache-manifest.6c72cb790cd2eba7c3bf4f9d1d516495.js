ad0afaeb3e5ca9aef97f70f9babb8a2de81f5e1f
weshsnr <ndungugeoffrey11@gmail.com>
2020-09-21 10:37:53 +0300

    Updates

---
+++ precache-manifest.6c72cb790cd2eba7c3bf4f9d1d516495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40e2cc8c1d615881581bb24898e0081",
+    "url": "/avengers-assemble/index.html"
+  },
+  {
+    "revision": "e5509fae59f3a0b7f55e",
+    "url": "/avengers-assemble/static/css/main.259652d0.chunk.css"
+  },
+  {
+    "revision": "a30408d7387599c2e3b4",
+    "url": "/avengers-assemble/static/js/2.a8513d88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avengers-assemble/static/js/2.a8513d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5509fae59f3a0b7f55e",
+    "url": "/avengers-assemble/static/js/main.e24a4dbf.chunk.js"
+  },
+  {
+    "revision": "cc487b14c1b3dd00124c",
+    "url": "/avengers-assemble/static/js/runtime-main.33c890e2.js"
+  }
+]);
